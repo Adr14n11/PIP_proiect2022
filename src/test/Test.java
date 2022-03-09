ba5ef714446e4c5cac58f0182d31f86a0875cd18
StefaniaPaduraru <stefania.paduraru@student.tuiasi.ro>
2022-03-09 12:55:04 +0200

    commit conflictual Stef

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,5 +5,9 @@ public class Test {
 	//test class
 	//added a comment after the initial commit
 	
+<<<<<<< HEAD
 	//comentariu pentru commit
+=======
+	//Added a modification from Adrian!!
+>>>>>>> refs/remotes/origin/test2
 }
