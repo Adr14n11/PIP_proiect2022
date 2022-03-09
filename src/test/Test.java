81dc0b12e4fb04727ed56abf32d2a5f70edeedc5
Adr14n11 <adrian.iordachescu@student.tuiasi.ro>
2022-03-09 13:00:53 +0200

    Added a thank you note inside the code, for the sfintisor

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,13 +3,12 @@ package test;
 public class Test {
 
 	//modificare de la flv eheh
-	//un sfintisor la petru e 5.5lei thank me later
+	//un sfintisor la petru e 5.5lei thank me later => Suuper, mersi!
 	
 	//test class
 	//added a comment after the initial commit
 	
-<<<<<<< HEAD
-=======
+
 	//Added a modification from Adrian!!
->>>>>>> 8d9c214a261aa7d17a4d9f27c2a9e2be661cc4b4
+
 }
