f20988365de66bd73b2d77b9a0c143049167f092
flv <b.flavia.ioana@gmail.com>
2022-04-20 13:40:02 +0300

    Tramvaie de interes

--- tramvaie/src/tramvaie/Main.java
+++ tramvaie/src/tramvaie/Main.java
@@ -12,9 +12,8 @@ public class Main {
 		//am creat cateva zone de interes
 		TramvaieDeInteres Cod2216_momentul1 = new TramvaieDeInteres("Tramvaiul 13", 47.148753, 27.620484); //tramvaiul1 in momentul de timp 1
 		TramvaieDeInteres Cod2216_momentul2 = new TramvaieDeInteres("Tramvaiul 13", 47.147880, 27.603558); //tramvaiul1 in momentuld e timp 2
-		
-		
-		
+		 
+			
 		
 		
 		// afisam elementele create:
