6490091aafb1304fb4de0fe7682b0aae34c0f23b
flv <b.flavia.ioana@gmail.com>
2022-04-27 17:13:17 +0300

    commit dupa code code review

--- tramvaie/src/tramvaie/Main.java
+++ tramvaie/src/tramvaie/Main.java
@@ -1,21 +1,13 @@
 package tramvaie;
 
-//importam libraria pentru vectori:
-//import java.util.Vector;
-
-
 public class Main {
 
 	public static void main(String[] args) {
-		//Vector<TramvaieDeInteres> v = new Vector<TramvaieDeInteres>();	// am creat un vector ce va primi elemente de tip TramvaieDeInteres
 		
 		//am creat cateva zone de interes
 		TramvaieDeInteres Cod2216_momentul1 = new TramvaieDeInteres("Tramvaiul 13", 47.148753, 27.620484); //tramvaiul1 in momentul de timp 1
 		TramvaieDeInteres Cod2216_momentul2 = new TramvaieDeInteres("Tramvaiul 13", 47.147880, 27.603558); //tramvaiul1 in momentuld e timp 2
 		 
-			
-		
-		
 		// afisam elementele create:
 		Cod2216_momentul1.afisare();
 		Cod2216_momentul2.afisare();
--- tramvaie/src/tramvaie/TramvaieDeInteres.java
+++ tramvaie/src/tramvaie/TramvaieDeInteres.java
@@ -4,8 +4,7 @@ public class TramvaieDeInteres  {
 	//in TramvaieDeInteres stocam datele legate de numele locatiei, latitudinea locatiei si longitudinea locatiei 
 	String locatie;						
 	double latitudine, longitudine;
-	
-	
+		
 	//am creat un constructor care va atribui campurilor valorile transmise prin argumente
 	TramvaieDeInteres(String locatie, double latitudine, double longitudine){
 		this.locatie = locatie;
@@ -13,7 +12,7 @@ public class TramvaieDeInteres  {
 		this.longitudine = longitudine;
 	}
 	
-	// functie de afisare:
+	
 	void afisare(){
 	
 		System.out.println("Locatie: " + locatie + ", Latitudine: "+ latitudine +", Longitudine: "+ longitudine);
